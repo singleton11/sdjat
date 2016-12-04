e3f5cdc991fc0319ff821b5c9ac71c423613d9d2
Anton Prokhorov <betrayer11@gmail.com>
2016-12-04 20:57:09 +0700

    Add ugettext_lazy for verbose_name of app

--- {{cookiecutter.app_slug}}/apps.py
+++ {{cookiecutter.app_slug}}/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
 
 class {{cookiecutter.app_camel_case}}Config(AppConfig):
     name = 'apps.{{cookiecutter.app_slug}}'
-    verbose_name = '{{cookiecutter.app_name}}'
+    verbose_name = _('{{cookiecutter.app_name}}')
